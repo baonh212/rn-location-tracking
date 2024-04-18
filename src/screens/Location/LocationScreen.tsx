f76304b6e626f04b31fffd175cd05e1a24e01058
baonh <baonh212@gmail.com>
2024-04-18 15:37:09 +0700

    feat: config expo-location

--- src/screens/Location/LocationScreen.tsx
+++ src/screens/Location/LocationScreen.tsx
@@ -1,13 +1,37 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {Animated, FlatList, StyleSheet, Text, View} from 'react-native';
 import {colors, metrics} from '../../themes';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 import {RectButton} from 'react-native-gesture-handler';
+import * as Location from 'expo-location';
+import {LocationObject} from 'expo-location';
+
+type AnimatedInterpolation = ReturnType<Animated.Value['interpolate']>;
 
 export const LocationScreen = () => {
   const abc = [1, 2, 3, 4];
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
-  const renderRightActions = (progress, dragX) => {
+  useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  console.log(location);
+
+  const renderRightActions = (
+    progress: AnimatedInterpolation,
+    dragX: AnimatedInterpolation,
+  ) => {
     const trans = dragX.interpolate({
       inputRange: [-80, 0],
       outputRange: [1, 0],
@@ -49,7 +73,7 @@ export const LocationScreen = () => {
     );
   };
 
-  const renderItem = useCallback(({item}) => {
+  const renderItem = useCallback(({item}: {item: any}) => {
     return (
       <Swipeable
         childrenContainerStyle={{
